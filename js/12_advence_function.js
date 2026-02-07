a4f4b90361145c73a6f256f12cfb59bdfb7260e5
Md Jobarul Islam <jobarulislam1203@gmail.com>
2026-02-07 20:05:54 +0600

    Add advanced functions and examples in js file

---
+++ js/12_advence_function.js
@@ -0,0 +1,51 @@
+    function greeting(){
+        console.log('hello');
+    }
+    greeting();
+    const num =2;
+    const function1 = function(){ 
+        console.log('hello2');
+
+    };
+    console.log(function1);
+    console.log(typeof function1);
+    function1();
+
+    const object1 ={
+    num : 2,
+    fun : function(){console.log('hello3');}
+    };
+
+    object1.fun();
+
+    function display(param){
+        console.log(param);
+    }
+
+    display(2);
+
+    function run(pra){
+    pra();
+    }
+    run(function(){
+    console.log('hello4');
+    });
+
+    setTimeout(function(){
+        console.log('timeout');
+        console.log('timeout2');
+    }, 2000);
+    console.log('next line');
+
+    setInterval(function(){console.log('interval')}, 3000);
+
+    console.log ('the');
+   [
+    'make dinner',
+    'wash dishes',
+    'watch tv'
+   ].forEach(function(value, index){
+    if(value === 'wash dishes'){return;}
+    console.log(index);
+    console.log(value); 
+   });
